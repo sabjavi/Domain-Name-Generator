85260e6ebe0d5b13048ec09bd0e025b110b724e2
ccarcache <ceciliocarcache@gmail.com>
2021-04-13 08:01:54 +0000

    Ejercicio dia 7 finalizado code name generator

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
-let pronoun = ["the", "our"];
-let adj = ["great", "big"];
-let noun = ["jogger", "racoon"];
-let domain = ["com", "net", "us"];
-
-let domainNames = [];
-let i = 0;
-pronoun.forEach((elementPronoun, indexPronoun) => {
-  adj.forEach((elementAdj, indexAdj) => {
-    noun.forEach((elementNoun, indexNoun) => {
-      domain.forEach((elementDomain, indexDomain) => {
-        domainNames[i] =
-          elementPronoun + elementAdj + elementNoun +"."+ elementDomain;
-
-        i++;
-      });
-    });
-  });
-});
-
-console.log(domainNames);
+let pronoun = ["the","our"];
+let adj = ["great", "big", "acid", "hungry"];
+let noun = ["jogger","racoon", "minimal", "micaela"];
+let domains = ["com","net","org","us", "es", "cl"];
+const createDomains = () => {
+    for (p in pronoun) {
+        for (a in adj) {
+            for (n in noun) {
+                for (d in domains){
+                    let str= `${pronoun[p]}${adj[a]}${noun[n]}`
+                    if(str.includes(domains[d])){
+                        let newStr = str.replace(domains[d], "");
+                        finalStr = `${newStr}.${domains[d]}`
+                        console.log(finalStr)
+                    } else {
+                        console.log(`${str}.${domains[d]}`)
+                    }
+                }
+            }
+        }
+    }
+}
+createDomains();
